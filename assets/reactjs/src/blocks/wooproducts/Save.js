132dd8afed8b91b82f3686f629b0f3cedeb07f2a
faisal29 <faisalcse29@gmail.com>
2019-11-15 17:00:20 +0600

    api call save view

--- assets/reactjs/src/blocks/wooproducts/Save.js
+++ assets/reactjs/src/blocks/wooproducts/Save.js
@@ -11,11 +11,33 @@ export default function Save(props) {
 		}
 	} = props
 
+
+	async function asyncfunction() {
+		let prodructs
+		await wp.apiFetch({ path: '/wc/blocks/products/categories' }).then(Response => prodructs = Response)
+		console.log('prodructs : ', prodructs)
+		return (
+			<div>
+				{
+					prodructs.map(({ name }) => <span>{name}</span>)
+				}
+			</div>
+
+		)
+
+	}
+
+
 	return (
 		<div className={`qubely-block-${uniqueId}`}>
+			{
+				asyncfunction()
+			}
 			<div className={`qubely-woo__product_wrapper`}>
 				<p>Init woo prodructs block</p>
+				<p>test</p>
 			</div>
+
 		</div>
 	)
 
