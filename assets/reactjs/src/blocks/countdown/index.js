006175ff1aaba86be6df9defc271d8d89f933391
delowardev <delowardev@gmail.com>
2020-02-18 18:04:34 +0600

    Css updated

--- assets/reactjs/src/blocks/countdown/index.js
+++ assets/reactjs/src/blocks/countdown/index.js
@@ -8,9 +8,9 @@ const { registerBlockType } = wp.blocks
 
 registerBlockType('qubely/countdown', {
     title: __('Countdown'),
-    description: __('Create stylish countdown blocks with Qubely Buttons.'),
+    description: __('Create under construction banners, announce release and more with the countdown block.'),
     category: 'qubely',
-    icon: <img src={qubely_admin.plugin + 'assets/img/blocks/block-countdown.svg'} alt={__('Countdown Block')} />,
+    icon: <img src={qubely_pro_admin.plugin + 'assets/img/blocks/block-countdown.svg'} alt={__('Countdown Block')} />,
     supports: {
         align: ['center', 'wide', 'full'],
     },
