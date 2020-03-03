c851cc3a05cf6fa27d7018ae0809945f93ab2a8f
faisalahmedbd <faisalcse29@gmail.com>
2020-03-03 16:05:27 +0600

    deprecated support in testimonial carousel

--- assets/reactjs/src/blocks/testimonialcarousel/Save.js
+++ assets/reactjs/src/blocks/testimonialcarousel/Save.js
@@ -1,37 +1,14 @@
-const { Component } = wp.element
-const { RichText } = wp.blockEditor
+import {
+  renderAuthorInfo
+} from './helpers';
+
+const { Component } = wp.element;
+const { RichText } = wp.blockEditor;
 const { HelperFunction: { animationAttr } } = wp.qubelyComponents
 class Save extends Component {
-  renderAvatar = (avatar, avatarAlt) => {
-    return (
-      <div className="qubely-testimonial-avatar-wrapper">
-        {avatar.url != undefined ?
-          <img className="qubely-testimonial-avatar" src={avatar.url} alt={avatarAlt} />
-          :
-          <div className="qubely-image-placeholder qubely-testimonial-avatar"><i className="far fa-user" /></div>
-        }
-      </div>
-    )
-  }
-  renderAuthorInfo = (item) => {
-    const { attributes: { layout, showAvatar, avatarAlt, avatarLayout } } = this.props
-    const { author, designation, avatar } = item
 
-    return (
-      <div className={`qubely-testimonial-author`}>
-        {(layout === 3 && showAvatar) && this.renderAvatar(avatar, avatarAlt)}
-        <div className={showAvatar ? `qubely-testimonial-avatar-layout-${avatarLayout}` : ``}>
-          {(layout !== 3 && showAvatar && (avatarLayout == 'left' || avatarLayout == 'top')) && this.renderAvatar(avatar, avatarAlt)}
-          <div className="qubely-testimonial-author-info">
-            <div className="qubely-testimonial-author-name"><RichText.Content value={author} /></div>
-            <div className="qubely-testimonial-author-designation"><RichText.Content value={designation} /></div>
-          </div>
-          {(layout !== 3 && showAvatar && (avatarLayout == 'right' || avatarLayout == 'bottom')) && this.renderAvatar(avatar, avatarAlt)}
-        </div>
-      </div>
-    )
-  }
-  renderTestimonial() {
+
+  renderTestimonial = () => {
     const { attributes: { carouselItems, showRatings, layout, quoteIcon } } = this.props
 
     return (carouselItems.map((item, index) => {
@@ -39,7 +16,7 @@ class Save extends Component {
       return (
         <div key={index} className={`qubely-carousel-item`}>
           <div className={`qubely-tesitmonial-item qubely-tesitmonial-item-layout-${layout}`}>
-            {layout == 2 && this.renderAuthorInfo(item)}
+            {layout == 2 && renderAuthorInfo(item, this.props.attributes)}
             {(quoteIcon && layout == 1) &&
               <div className="qubely-testimonial-quote">
                 <span className={`qubely-quote-icon ${quoteIcon}`} />
@@ -52,7 +29,7 @@ class Save extends Component {
               <div className="qubely-testimonial-content"><RichText.Content value={message} /></div>
               {(showRatings && ratings > 0 && layout == 1) && <div className="qubely-testimonial-ratings" data-qubelyrating={ratings} />}
             </div>
-            {layout !== 2 && this.renderAuthorInfo(item)}
+            {layout !== 2 && renderAuthorInfo(item,this.props.attributes)}
             {(quoteIcon && layout == 2) &&
               <div className="qubely-testimonial-quote qubely-position-bottom">
                 <span className={`qubely-quote-icon ${quoteIcon}`} />
@@ -65,7 +42,27 @@ class Save extends Component {
   }
 
   render() {
-    const { attributes: { uniqueId, layout, items, sliderItemMargin, autoPlay, arrowStyle, infiniteLoop, activeFade, isCentered, dragable, nav, dots, dotIndicator, interval, speed, animation } } = this.props
+    const {
+      attributes: {
+        uniqueId,
+        layout,
+        items,
+        sliderItemMargin,
+        autoPlay,
+        arrowStyle,
+        infiniteLoop,
+        activeFade,
+        isCentered,
+        dragable,
+        nav,
+        dots,
+        dotIndicator,
+        interval,
+        speed,
+        animation
+      }
+    } = this.props;
+
     let options = JSON.stringify({
       autoplay: autoPlay,
       items: items,
@@ -94,8 +91,8 @@ class Save extends Component {
           items: items.xs
         }
       ],
-    }
-    )
+    });
+
     return (
       <div className={`qubely-block-${uniqueId}`} {...animationAttr(animation)}>
         <div className={`qubely-block-testimonial-carousel qubely-testimonial-carousel-layout-${layout}`}>
