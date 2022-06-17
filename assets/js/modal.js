fe418670a7fda78e155c97230b3811eec6188f92
Sazedul Haque <sazedulhaque5@gmail.com>
2022-06-17 11:38:09 +0600

    FIX: CSS and JS update

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    const modalPopups = document.querySelectorAll(".qubely-modal-popup");
     const modalButtons = document.querySelectorAll(".qubely-block-modal-link");
     const modalCloseButtons = document.querySelectorAll(".qubely-block-modal-close-btn");
-
-    // Make display none initially
-    modalPopups.forEach((modal) => {
-        modal.setAttribute("style", "display:none;");
-    });
 
     // Handle modal button click event
     modalButtons.forEach((button) => {
@@ -21,9 +15,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
     modalCloseButtons.forEach((button) => {
         button.addEventListener("click", (event) => {
             const closeAnimationName = button.closest(".qubely-modal-box").getAttribute("closeAnimation");
-            const openAnimationName = button.closest(".qubely-modal-box").style["animation-name"];
-            button.closest(".qubely-modal-box").setAttribute("style", `animation-name:${closeAnimationName};`);
-            button.closest(".qubely-modal-popup").setAttribute("style", "opacity:0;");
+            const openAnimationName = button.closest(".qubely-modal-box").style.animationName;
+            button.closest(".qubely-modal-box").style.animationName = closeAnimationName;
+            button.closest(".qubely-modal-popup").style.opacity = 0;
             setTimeout(() => {
                 button.closest(".qubely-modal-popup").setAttribute("style", "display:none;");
                 button.closest(".wp-block-qubely-modal").setAttribute("style", "z-index:0;");
@@ -41,9 +35,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 if (mainWrapper) {
                     if (!modalBox.contains(event.target) && event.target !== mainWrapper.querySelector(".qubely-block-modal-link")) {
                         const closeAnimationName = modalBox.getAttribute("closeAnimation");
-                        const openAnimationName = modalBox.style["animation-name"];
-                        modalBox.setAttribute("style", `animation-name:${closeAnimationName};`);
-                        modalBox.closest(".qubely-modal-popup").setAttribute("style", "opacity:0;");
+                        const openAnimationName = modalBox.style.animationName;
+                        modalBox.style.animationName = closeAnimationName;
+                        modalBox.closest(".qubely-modal-popup").style.opacity = 0;
                         setTimeout(() => {
                             modalBox.closest(".qubely-modal-popup").setAttribute("style", "display:none;");
                             modalBox.closest(".wp-block-qubely-modal").setAttribute("style", "z-index:0;");
